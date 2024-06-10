9cc2e5e19e63b333042b2d5b6cfe04e01a5f154c
Erum Sikander <mrsikanderghouri@gmail.com>
2024-06-10 05:32:32 +0500

    hlaf worked

---
+++ new.js
@@ -0,0 +1,5 @@
+"use strict";
+// Sir-Zia-repo-04   practice:
+// Strong typing define:
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Erum Sikander");
---
+++ new.ts
@@ -0,0 +1,6 @@
+
+
+// Sir-Zia-repo-04   practice:
+// Strong typing define:
+
+console.log("Erum Sikander")
